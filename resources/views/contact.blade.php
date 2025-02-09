5a127c13cd6f998a5d6fed8ddb934eda5904f21b
ButchNinoButas <butchbutas69@gmail.com>
2025-02-09 18:26:37 +0800

    0208_4_make-it-pretty-using-tailwindcss

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Page.</title>
-</head>
-<body>
-    <h1>Contact Page.</h1>
-</body>
-</html>
+<x-layout>
+<x-slot:heading>
+    Contact Page
+</x-slot:heading>
+
+    <h1>Hello from the Contact Page.</h1>
+</x-layout>
+
